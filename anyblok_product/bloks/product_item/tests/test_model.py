7fd79ffc83d3beec162c0d4e540ca55da45f60a8
Franck Bret <f.bret@sensee.com>
2018-02-24 14:38:29 +0100

    flake8

--- anyblok_product/bloks/product_item/tests/test_model.py
+++ anyblok_product/bloks/product_item/tests/test_model.py
@@ -13,10 +13,29 @@ class TestProductModel(BlokTestCase):
         self.assertEqual(prod.sku, "sku001")
 
     def test_product_creation_properties(self):
+        Item = self.registry.Product.Item
         props = dict(color="Blue", size="L")
-        prod = self.registry.Product.Item.insert(sku="sku001", name="Foo",
-                                                 properties=props,
-                                                 description="Foo description")
-        self.assertEqual(prod.properties['color'], "Blue")
+        prod = Item.insert(
+                sku="sku001", name="Foo", properties=props,
+                description="Foo description")
+
         self.assertIn("color", prod.properties.keys())
+        self.assertNotIn("unexisting_property", prod.properties.keys())
+
         self.assertIn("Blue", prod.properties.values())
+        self.assertNotIn("unexisting_value", prod.properties.values())
+
+        self.assertEqual(prod.properties['color'], "Blue")
+        self.assertEqual(
+            Item.query().filter(
+                Item.properties['color'].astext == 'Red'
+            ).count(), 0)
+        self.assertEqual(
+            Item.query().filter(
+                Item.properties['color'].astext == 'Blue'
+            ).count(), 1)
+
+        self.assertEqual(
+            Item.query().filter(
+                Item.properties['unexisting_property'].astext == 'Blue'
+            ).count(), 0)
